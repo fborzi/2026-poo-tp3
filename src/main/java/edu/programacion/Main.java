556644750d2d36def74d8da611c99d534c6ec22c
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-10 21:00:13 -0300

    [TP-POO-3] - {feat} : se realiza carga de ejercicio 2302

--- src/main/java/edu/programacion/Main.java
+++ src/main/java/edu/programacion/Main.java
@@ -1,8 +1,10 @@
 package edu.programacion;
 
+import edu.programacion.ejercicio2302.Contador;
+
 public class Main {
     static void main() {
-
+        Contador contador = new Contador();
     }
 }
 
--- src/test/java/edu/programacionImpl/ContadorTest.java
+++ src/test/java/edu/programacionImpl/ContadorTest.java
@@ -1,5 +1,6 @@
 package edu.programacionImpl;
 
+import edu.programacion.ejercicio2302.Contador;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
