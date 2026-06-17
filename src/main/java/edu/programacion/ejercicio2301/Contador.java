12f5ff60acaaf0038a806383cdfd5a6861edf059
vann <ivanjtomicic@gmai.com>
2026-06-17 20:56:14 -0300

    ejercicio 2301

---
+++ src/main/java/edu/programacion/ejercicio2301/Contador.java
@@ -0,0 +1,39 @@
+package edu.programacion.ejercicio2301;
+
+public class Contador {
+    //atributo
+    private Integer valor;
+
+    //constructor
+    public Contador(){}
+
+
+
+    public Integer iniciar(Integer n){
+        setValor(n);
+        return getValor();
+    }
+
+    public void setValor(Integer valor){
+        this.valor = valor;
+    }
+
+
+    public Integer sumar() {
+        setValor(getValor() + 1);
+        return getValor();
+    }
+
+    public Integer restar(Integer n){
+        setValor(getValor() - n);
+        return getValor();
+    }
+
+    public Integer getValor(){
+        return this.valor;
+    }
+
+
+
+}
+
