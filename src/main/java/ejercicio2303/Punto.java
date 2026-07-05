027f0953a796a297a40a265b14d75299b38974f5
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-07-05 20:58:04 -0300

    ejercicio 2303, implemente una clase punto, declare los atributos. desarrolle los metodos para sumar un alor y calcular las distanciasentre dos puntos. subo el ejercicio para revisar

---
+++ src/main/java/ejercicio2303/Punto.java
@@ -0,0 +1,38 @@
+public class Punto {
+
+    private double x;
+    private double y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void sumarValor(double unValor) {
+        x += unValor;
+        y += unValor;
+    }
+
+    public void sumarPunto(Punto unPunto) {
+        x += unPunto.x;
+        y += unPunto.y;
+    }
+
+    public double distanciaDe(Punto unPunto) {
+        double dx = unPunto.x - x;
+        double dy = unPunto.y - y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double distanciaAlOrigen() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
