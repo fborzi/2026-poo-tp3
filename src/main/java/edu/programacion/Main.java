8525ea9de366950a4441e701b398116f283852f9
Lautaro Tamola <tamolalautaro@gmail.com>
2026-07-05 21:41:07 -0300

    [TP-POO-003] - {fix} : se realiza carga de datos en main

--- src/main/java/edu/programacion/Main.java
+++ src/main/java/edu/programacion/Main.java
@@ -8,15 +8,24 @@ import edu.programacion.ejercicio2306.Cita;
 import edu.programacion.ejercicio2306.Lugar;
 import edu.programacion.ejercicio2306.Persona;
 
+import java.time.LocalDate;
+
 public class Main {
     static void main() {
         Contador contador = new Contador();
         AireAcondicionado aireAcondicionado = new AireAcondicionado();
-        Punto punto = new Punto();
-        Circulo circulo = new Circulo();
-        Cita cita = new Cita();
-        Persona persona = new Persona();
-        Lugar lugar = new Lugar();
+        Punto punto = new Punto(0.0, 0.0);
+        Circulo circulo = new Circulo(punto, 10.0);
+        Cita cita = new Cita(
+                LocalDate.now(),
+                Persona,
+                Lugar,
+                5);
+        Persona persona = new Persona("Lautaro", "245777524", "klautaro@gmail.com");
+        Lugar lugar = new Lugar(    "Cafetería",
+                "Av. Siempre Viva 123",
+                "Buenos Aires",
+                1000);
 
 
     }
