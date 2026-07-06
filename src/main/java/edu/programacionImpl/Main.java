8ca9b15615bdb12261e4e80228ca0d873e4e4472
Carlos Avalle Vezzoni <avallecarlos1@gmail.com>
2026-07-05 22:07:04 -0300

    [ejercicio2301 al 2307] Agregue clase Main para probar todas las clases. Crea un AireAcondicionado, lo enciende y prueba subir y bajar la temperatura verificando que respeta los limites. Crea una Persona, un Lugar y una Cita con fecha, contacto, lugar e importancia. Crea las tres guitarras y verifica con instanceof que cada una es reconocida como Guitarra.

--- src/main/java/edu/programacionImpl/Main.java
+++ src/main/java/edu/programacionImpl/Main.java
@@ -1,5 +1,62 @@
 package edu.programacionImpl;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("AIRE ACONDICIONADO");
+        AireAcondicionado aire = new AireAcondicionado("Split", 30, 16);
+        System.out.println("Encendido: " + aire.encender(true));
+        System.out.println("Temperatura inicial: " + aire.consultarTemperaturaActual());
+        System.out.println("Subir 5: " + aire.subirTemperatura(5));
+        System.out.println("Subir 20 (supera el maximo): " + aire.subirTemperatura(20));
+        System.out.println("Bajar 3: " + aire.bajarTemperatura(3));
+        System.out.println("Bajar 50 (supera el minimo): " + aire.bajarTemperatura(50));
+        System.out.println("Modo fast: " + aire.encenderApagarModFast(true));
+        System.out.println("Modo low: " + aire.encenderApagarModLow(false));
+        System.out.println("Apagado: " + aire.apagar(false));
+
+        System.out.println("PERSONA");
+        Persona persona = new Persona();
+        persona.saludar();
+        persona.despedir();
+        persona.preguntarEstadoCivil();
+
+        System.out.println("CITA");
+        Lugar lugar = new Lugar();
+        Cita cita = new Cita();
+        cita.setFecha(LocalDate.of(2026, 7, 10));
+        cita.setContacto(persona);
+        cita.setLugar(lugar);
+        cita.setImportancia(4);
+        System.out.println("Fecha: " + cita.getFecha());
+        System.out.println("Importancia: " + cita.getImportancia());
+        System.out.println("Tiene contacto: " + (cita.getContacto() != null));
+        System.out.println("Tiene lugar: " + (cita.getLugar() != null));
+
+        System.out.println("GUITARRAS");
+
+        GuitarraCriolla criolla = new GuitarraCriolla();
+        System.out.println("Criolla es Guitarra: " + (criolla instanceof Guitarra));
+        criolla.afinar();
+        criolla.tocarChamame();
+        criolla.tocarFolklore();
+        criolla.cambiarCuerdasNylon();
+        criolla.regularTension();
+
+        GuitarraElectrica electrica = new GuitarraElectrica();
+        System.out.println("Electrica es Guitarra: " + (electrica instanceof Guitarra));
+        electrica.afinar();
+        electrica.conectarAmplificador();
+        electrica.ajustarDistorsion();
+        electrica.cambiarMaster();
+
+        GuitarraAcustica acustica = new GuitarraAcustica();
+        System.out.println("Acustica es Guitarra: " + (acustica instanceof Guitarra));
+        acustica.afinar();
+        acustica.ajustarCejilla();
+        acustica.regularAlturaCuerda();
+        acustica.ajustarVolumenNatural();
     }
 }
